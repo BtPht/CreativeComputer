8f3cf1d7f3d83de63af2e20bb84cb824d21b3b85
Bertrand Pechenot <bertrand.pechenot@gmail.com>
2014-01-14 14:30:16 +0100

    création d'une classe Weather

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "testApp.h"
+#include "Weather.h"
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
+  
+	Weather test("London");
 
 	ofSetupOpenGL(1200, 500, OF_WINDOW);
 	ofRunApp(new testApp());
