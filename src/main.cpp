fe71811c5103550585f7659c7ff6ea63e7951196
Dorian <ouvrard.dorian@gmail.com>
2014-01-14 13:39:19 +0100

    -Remise en forme du code -Ajout de la fonction contourPainting(x) : dessine les contours selon un seuil fixe x -Ajout de la fonction allContourPainting() : dessine les contours selon une plage de valeurs du seuil

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main() {
 
-	ofSetupOpenGL(1200, 500, OF_WINDOW);
+	ofSetupOpenGL(800, 500, OF_WINDOW);
 	ofRunApp(new testApp());
 }
--- src/testApp.cpp
+++ src/testApp.cpp
@@ -5,79 +5,28 @@ using namespace cv;
 using namespace std;
 
 void testApp::setup() {
-	cam.initGrabber(ofGetWidth()/3,ofGetHeight());
 
 	ofSetVerticalSync(true); 
 	ofEnableSmoothing(); 
 
+	painting.loadImage("img/tree.jpg");
+	painting.resize(400,500);
+
 	marking = NULL;
 	brush = NULL;
 	paint = NULL;
+	it=0;
 
 }
 
 void testApp::update() {
 
-	cam.update();
-	if(cam.isFrameNew()) {
-		convertColor(cam, gray, CV_RGB2GRAY);
-		Canny(gray, edge,mouseX,mouseY, 3);
-		edge.update();
-	}
-
-	if(marking != NULL)
-    {
-        marking->update();
-    }
-
-    for(auto i = 0u; i < markings.size(); i++)
-    {
-        markings[i]->update();
-    }
-
-	if(cam.isFrameNew()) {
-		contourFinder.setThreshold(ofMap(mouseX, 0, ofGetWidth(), 0, 255));
-		contourFinder.findContours(cam);
-	}
-
-	vector<vector<cv::Point> > contours = contourFinder.getContours();
-
-
-	for(auto &m : markings)
-		delete m;
-
-	markings.clear();
-
-	for(auto &c : contours){
-
-		ofColor color = ofColor::black;
-	
-		path = new ofxSuperPath();
-		path->setPathPressureType(OFX_PATH_PRESSURE_FIXED,10);
-		paint = new ofxPaint(path, color,100);
-		brush = new ofxRibbonBrush(path, paint);
-		brush->setDynamic(false);
-
-		marking = new ofxMarking(path, paint, brush);
-		path->reset();
-		path->lineStart(c[0].x,c[0].y,0,color, ofGetFrameNum(), 0);
-
-		for(auto &p : c){
-			path->lineTo(p.x,p.y,0,color, ofGetFrameNum(), 0);
-		}
-
-		marking->pathFinished();
-		markings.push_back(marking);
-		marking = NULL;
-	}
+	contourPainting(it);
+	it++;
+	cout << it << endl;
 }
 
 void testApp::draw() {
-
-	//cam.draw(0, 0);
-	edge.draw(ofGetWidth()/3, 0);
-//	contourFinder.draw();
-
 
 	if(marking != NULL)
     {
@@ -89,8 +38,78 @@ void testApp::draw() {
         markings[i]->draw();
     }
 
-	
+	painting.draw(ofGetWidth()/2,0);
 
+}
+
+void testApp::contourPainting(int x){
+
+	contourFinder.setThreshold(ofMap(x, 0, ofGetWidth(), 0, 255));
+	contourFinder.findContours(painting);
+
+	for(auto &c : contourFinder.getContours()){
+
+		ofColor color = ofColor::black;
+
+		path = new ofxSuperPath();
+		path->setPathPressureType(OFX_PATH_PRESSURE_FIXED,10);
+		paint = new ofxPaint(path, color,2);
+		brush = new ofxRibbonBrush(path, paint);
+		brush->setDynamic(false);
+
+		marking = new ofxMarking(path, paint, brush);
+		path->reset();
+		path->lineStart(c[0].x,c[0].y,0,color, ofGetFrameNum(), 0);
+
+		for(auto &p : c){
+			path->lineTo(p.x,p.y,0,color, ofGetFrameNum(), 0);
+		}
+
+		marking->pathFinished();		
+		markings.push_back(marking);
+		marking = NULL;
+	}
+}
+
+void testApp::allContourPainting(){
+
+	for(int i=0; i<500; i++){
+		contourFinder.setThreshold(ofMap(i, 0, ofGetWidth(), 0, 255));
+		contourFinder.findContours(painting);
+
+		allContours.push_back(contourFinder.getContours());
+	}
+	
+	for(auto &m : markings)
+		delete m;
+
+	markings.clear();
+
+	for(auto &all : allContours){
+
+		for(auto &c : all){
+
+			ofColor color = ofColor::black;
+	
+			path = new ofxSuperPath();
+			path->setPathPressureType(OFX_PATH_PRESSURE_FIXED,10);
+			paint = new ofxPaint(path, color,2);
+			brush = new ofxRibbonBrush(path, paint);
+			brush->setDynamic(false);
+
+			marking = new ofxMarking(path, paint, brush);
+			path->reset();
+			path->lineStart(c[0].x,c[0].y,0,color, ofGetFrameNum(), 0);
+
+			for(auto &p : c){
+				path->lineTo(p.x,p.y,0,color, ofGetFrameNum(), 0);
+			}
+
+			marking->pathFinished();		
+			markings.push_back(marking);
+			marking = NULL;
+		}
+	}
 }
 
 void testApp::exit()
