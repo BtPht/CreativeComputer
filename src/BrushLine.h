f7d1c9275cdec4a0c9dcdbf7f66782adf720dce4
Bertrand Pechenot <bertrand.pechenot@gmail.com>
2014-02-12 13:52:37 +0100

    correction de la valeur par défaut qui était à NULL pour une variable non pointeur

--- src/BrushLine.h
+++ src/BrushLine.h
@@ -8,15 +8,15 @@ public:
 
 	BrushLine(ofColor _color=ofColor::black,int thickness=10,int opacity=2);
 
-	void lineTo(int x,int y,ofColor _color=NULL);
+	void lineTo(int x,int y,ofColor _color=ofFloatColor(0));
 	void lineEnd(vector<ofxMarking *>* markings);
 	void lineEnd();
 
 	ofColor color;
 	ofxMarking* marking;
-    ofxSuperPath* path;
-    ofxDynamicPalettePaint* paint;
-   	ofxRibbonBrush* brush;
+	ofxSuperPath* path;
+	ofxDynamicPalettePaint* paint;
+	ofxRibbonBrush* brush;
 	vector<ofColor>* colors;
 };
 
