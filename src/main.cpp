ea98197adf117db4683a155c0db488a18be28639
Bertrand Pechenot <bertrand.pechenot@gmail.com>
2014-02-16 18:03:28 +0100

    documentation du main

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,9 @@
 int main(int argc, char* argv[]) {
 
 	ofSetupOpenGL(1000, 600, OF_WINDOW);
+	
+	/* Instancier testApp qui est dérivé de ofbaseApp entraine le lancement de la boucle
+	 * principale de OpenFrameworks
+	 */
 	ofRunApp(new testApp());
 }
